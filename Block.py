e0703be79b3c109a6f288ae3b0e79dd49391757a
Maiquel Leonel <skywishmtfk@gmail.com>
2019-01-21 01:29:34 -0200

    aula 3

--- Block.py
+++ Block.py
@@ -1,13 +1,16 @@
 import hashlib
 import time
+import binascii
 
 class Block:
-    def __init__(self, index, previousHash, timestamp, data, hash):
+    def __init__(self, index, previousHash, timestamp, data, hash, difficulty, nonce):
         self.index = index
         self.previousHash = previousHash
         self.timestamp = timestamp
         self.data = data
         self.hash = hash
+        self.difficulty = difficulty
+        self.nonce = nonce
 
 class Blockchain:
     def __init__(self, genisisBlock):
@@ -36,9 +39,24 @@ class Blockchain:
             return False
         return True
 
-def calculateHash(index, previousHash, timestamp, data):
-    hashData = (str(index) + previousHash + str(timestamp) + data).encode('utf-8')
-    return hashlib.sha256().hexdigest()
+    def hashMatchesDifficulty(self, hash, difficulty):
+        hashBin = binascii.unhexlify(hash)
+        requiredPrefix = '0' * int(difficulty)
+        return hashBin.startsWith(requiredPrefix)
+
+    def findBlock(self, index, previousHash, timestamp, data, difficulty):
+        nonce = 0
+        while True:
+            hash = calculateHash(index, previousHash, timestamp, data, difficulty, nonce)
+            if self.hashMatchesDifficulty(hash, difficulty):
+                block = Block(index, previousHash, timestamp, data, difficulty, nonce)
+                return Block
+            nonce = nonce + 1
+
+
+def calculateHash(index, previousHash, timestamp, data, difficulty, nonce):
+    hashData = (str(index) + previousHash + str(timestamp) + data + str(difficulty) + str(nonce)).encode('utf-8')
+    return hashlib.sha256(hashData).hexdigest()
 
 ts = int(round(time.time() * 1000))
-genesisBlock = Block(0, "", ts, "Genesis Block!", calculateHash(0, "", ts, "Genesis Block!"))
+genesisBlock = Block(0, "", ts, "Genesis Block!", calculateHash(0, "", ts, "Genesis Block!", 1, 0), 1, 0)
