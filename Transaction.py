80b11fd10ad38778ff3a4d1f8fceb502746744fa
Maiquel Leonel <skywishmtfk@gmail.com>
2019-02-24 14:40:20 -0300

    aula 6

--- Transaction.py
+++ Transaction.py
@@ -18,6 +18,34 @@ class Input:
         self.outputIndex = None
         self.signature = None
 
+class UnspentOutput:
+    def __init__(self, outputId, outputIndex, address, amount):
+        self.outputId = outputId
+        self.outputIndex = outputIndex
+        self.address = address
+        self.amount = amount
+
+class UnspentOutputs:
+    def __init__(self):
+        self.__uxto = []
+        
+    def updateListUtxo(self, list):
+        self.__utxo = list
+
+    def newUnspentOutputs(self, transactions):
+        list = []
+        for transaction in transactions:
+            for inpt in transaction.input:
+                utxo = UnspentOutput(transaction.id, inpt.outputId, inpt.outputIndex, inpt.address, inpt.amount)
+                list.append(utxo)
+        self.updateListUtxo(list)
+
+def findUnspentOutput(outputId, outputIndex, listUnspentOutputs):
+    for utxo in listUnspentOutputs:
+        if utxo.outputId == outputId and utxo.outputIndex == outputIndex:
+            return True
+    return False
+
 def idTransaction(transaction):
     inputContents = ""
     outputContents = ""
@@ -26,4 +54,4 @@ def idTransaction(transaction):
     for output in transaction.output:
         outputContents += (output.address + output.amount)
     newIndexContent = (str(inputContents) + str(outputContents)).encode('utf-8')
-    return haslib.sha256(newIndexContent).hexdigest()
+    return hashlib.sha256(newIndexContent).hexdigest()
